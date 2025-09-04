bfce87ab87e0a6b57b4b02b929c31a592aaaf570
Zaqueu-Dev-Dias <zaqueudev0@gmail.com>
2025-09-03 22:08:50 -0300

    funcao

--- pasta_app/lib/main.dart
+++ pasta_app/lib/main.dart
@@ -10,11 +10,52 @@ void main() {
         child: Material(
           child: Column(
             children: [
-              AppBar(title: Text('MPCBUP'),),
-              TextField(decoration: InputDecoration(label: Row(children: [Text('Nome do produto')],)),),
-              TextField(decoration: InputDecoration(label: Row(children: [Text('Preço do mês anterio')],)),),
-              TextField(decoration: InputDecoration(label: Row(children: [Text('Preço do mês atual')],)),),
-              GestureDetector(child: Container(width: 200, height: 100,child: Text('Analisar', style: TextStyle(fontSize: 35),),),),
+              AppBar(title: Text('MPCBUP')),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    label: Row(children: [Text('Nome do produto')]),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(width: 3, color: Colors.black,)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    label: Row(children: [Text('Preço do mês anterio')]),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(width: 3, color: Colors.black,)),
+                ),
+              ),),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      label: Row(children: [Text('Preço do mês atual')]),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(width: 3, color: Colors.black,)),
+                    ),
+                  ),
+                ),
+              Padding(
+                padding: const EdgeInsets.only(top: 200),
+                child: GestureDetector(
+                  child: Container(
+                    width: 200,
+                    height: 70,
+                    decoration: BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(5)),
+                    child: Center(
+                      child: Text(
+                        'Analisar',
+                        style: TextStyle(fontSize: 35, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
